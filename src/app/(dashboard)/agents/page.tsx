2bcd853919998ae166231ca48bd00b79c9532517
Daniel Dao <danieldkdao@gmail.com>
2026-01-15 13:04:34 -0600

    10: agents data table

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -1,11 +1,43 @@
-import { AgentView } from "@/modules/agents/ui/views/agent-view";
+import {
+  AgentView,
+  AgentViewError,
+  AgentViewLoading,
+} from "@/modules/agents/ui/views/agent-view";
 import { getQueryClient, trpc } from "@/trpc/server";
+import { ErrorBoundary } from "react-error-boundary";
+import { Suspense } from "react";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { AgentHeader } from "@/modules/agents/ui/components/agent-header";
 
-const AgentsPage = () => {
+type AgentsPageProps = Promise<{
+  query?: string;
+  page?: string;
+}>;
+
+const AgentsPage = async ({
+  searchParams,
+}: {
+  searchParams: AgentsPageProps;
+}) => {
+  const { query, page } = await searchParams;
+  const parsedPage = Number(page) || 1;
   const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions({}));
+  void queryClient.prefetchQuery(
+    trpc.agents.getMany.queryOptions({ search: query, page: parsedPage })
+  );
 
-  return <AgentView />;
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <div className="w-full h-svh p-5 flex flex-col gap-4">
+        <AgentHeader />
+        <Suspense fallback={<AgentViewLoading />}>
+          <ErrorBoundary fallback={<AgentViewError />}>
+            <AgentView query={query} page={parsedPage} />
+          </ErrorBoundary>
+        </Suspense>
+      </div>
+    </HydrationBoundary>
+  );
 };
 
 export default AgentsPage;
--- src/modules/agents/ui/views/agent-view.tsx
+++ src/modules/agents/ui/views/agent-view.tsx
@@ -1,19 +1,50 @@
 "use client";
 
-import { useTRPC } from "@/trpc/client";
-import { CreateAgentFormModal } from "../components/create-agent-form-modal";
+import { LoadingState } from "@/components/loading-state";
+import { columns } from "../components/columns";
+import { DataTable } from "../components/data-table";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { useTRPC } from "@/trpc/client";
+import { ErrorState } from "@/components/error-state";
+import { EmptyState } from "@/components/empty-state";
 
-export const AgentView = () => {
+type AgentViewProps = {
+  query: string | undefined;
+  page: number;
+};
+
+export const AgentView = ({ query, page }: AgentViewProps) => {
   const trpc = useTRPC();
-  const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions({}));
+  const { data: agents } = useSuspenseQuery(
+    trpc.agents.getMany.queryOptions({ search: query, page })
+  );
   return (
-    <div className="w-full h-svh p-5">
-      <div className="flex items-center justify-between">
-        <h1 className="text-xl md:text-2xl font-medium">My Agents</h1>
-        <CreateAgentFormModal />
-      </div>
-      <div>{JSON.stringify(data)}</div>
+    <div className="flex-1 pb-4 flex flex-col gap-y-4">
+      <DataTable data={agents.agents} columns={columns} />
+      {agents.agents.length === 0 && (
+        <EmptyState
+          title="Create your first agent"
+          description="Create an agent to join your meetings. Each agent will follow your instructions and can interact with participants during the call."
+        />
+      )}
     </div>
+  );
+};
+
+export const AgentViewLoading = () => {
+  return (
+    <LoadingState
+      title="Loading Agents"
+      description="This may take a few seconds"
+    />
+  );
+};
+
+export const AgentViewError = () => {
+  return (
+    <ErrorState
+      title="Error loading agents"
+      description="Please try again later."
+    />
   );
 };
